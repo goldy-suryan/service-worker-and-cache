301f45a359e466e6908051ad19f233de6c36b0e9
goldy <goldysuryan87@gmail.com>
2018-08-25 03:15:37 +0530

    caching added

--- sw.js
+++ sw.js
@@ -1,16 +1,66 @@
+const version = 'v2';
+
 self.addEventListener('install', (e) => {
-    console.log('SW Installed');
+    e.waitUntil(
+        caches.open(version)
+            .then((cache) => {
+                return cache.addAll([
+                    '/css/style.css',
+                    '/about.html',
+                    '/contact.html',
+                    '/index.html',
+                    '/offline.html',
+                    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+                    '//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css',
+                    'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+                    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
+                ])
+            })
+    )
 });
 
 self.addEventListener('activate', e => {
-    console.log('SW Activated');
-});
+    e.waitUntil(
+        caches.keys()
+            .then((keys) => {
+                keys.map(key => {
+                    if (keys.indexOf(version) !== -1) {
+                        return caches.delete(key);
+                    }
+                })
+            })
+    )
+})
 
 self.addEventListener('fetch', e => {
 
-    if (!navigator.onLine) {
-        e.respondWith(new Response(' <h1><center>Offline</center></h1>', { headers: { 'content-type': 'text/html' } }));
-    } else {
-        e.respondWith(fetch(e.request));
-    }
-});
+    e.respondWith(caches.match(e.request)
+        .then(res => {
+            if (res) {
+                return res;
+            }
+
+            if (!navigator.onLine) {
+                return caches.match(new Request('/offline.html'));
+            }
+
+            var fetchRequest = e.request.clone();
+
+            return fetch(fetchRequest)
+                .then(response => {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    var responseToCache = response.clone();
+
+                    caches.open(version)
+                        .then((cache) => {
+                            cache.put(e.request, responseToCache)
+                        })
+
+                    return response;
+                })
+        })
+    )
+})
